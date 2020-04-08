730595f1992f8a46119f42eab15bb3340f041a60
Pedro [kaled] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2020-04-08 17:21:20 -0300

    Blank lines and tabs organization Rename some variables names

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports and Boot
 
 import pygame
+
 pygame.init()
 
 # Load
@@ -36,17 +37,18 @@ pygame.display.set_caption("The Game")
 font = pygame.font.Font('freesansbold.ttf', 16)
 clock = pygame.time.Clock()  # FPS
 
-# Characther
 
-class characther:
+# Character
+
+class Character:
     # Position
     xPos, yPos = 100, 415
-    
+
     # Stats
     isSuper = False
     inJump = False
     isFalling = False
-    isRunnig = False
+    isRunning = False
     faceDirection = 'RIGHT'
     stepCount = 0
     stayCount = 0
@@ -55,70 +57,71 @@ class characther:
     energy = 0
     coins = 0
     hp = 6
-    lifes = 3
-    
+    life = 3
+
     # Color
     color = 255, 255, 255
-    
+
     # Size
     size = w, h = 40, 65
-    
+
     # New
-    def _init_(self, x = xPos, y = yPos, w = size[0], h = size[1], s = speed):
+    def _init_(self, x=xPos, y=yPos, w=size[0], h=size[1], s=speed):
         self.xPos = x
         self.yPos = y
         self.w = w
         self.h = h
         self.size = w, h
         self.speed = s
-    
+
     # Gets
     def get_all(self):
         return self.xPos, self.yPos, self.w, self.h
-    
+
     # Sets
     def set_color(self, c: tuple = ()):
         if len(c) == 3:
             self.color = c
         else:
-            print('Isso num é uma cor!')
-    
+            print('Isso não é uma cor!')
+
     def jump(self):
         self.inJump = True
-    
+
     def left(self):
-        self.isRunnig = True
+        self.isRunning = True
         self.faceDirection = 'LEFT'
         self.walking_left()
-    
+
     def step(self):
-        if self.stepCount +1 >= 4:
+        if self.stepCount + 1 >= 4:
             self.stepCount = 0
         else:
             self.stepCount += 1
-    
+
     def right(self):
-        self.isRunnig = True
+        self.isRunning = True
         self.faceDirection = 'RIGHT'
         self.walking_right()
-    
+
     def stay(self):
-        self.isRunnig = False
+        self.isRunning = False
         self.stepCount = 0
-        if self.stayCount +1 >= 4:
+        if self.stayCount + 1 >= 4:
             self.stayCount = 0
         else:
             self.stayCount += 1
-    
+
     def super(self):
         self.isSuper = not self.isSuper
-    
+
     # Move
     def walking_left(self, t=1):
         self.xPos -= self.speed * t
-    
+
     def walking_right(self, t=1):
         self.xPos += self.speed * t
+
     '''
     def up(self, t=1):
         self.yPos -= self.speed * t
@@ -126,44 +129,45 @@ class characther:
     def down(self, t=1):
         self.yPos += self.speed * t
     '''
+
     def jumping(self, c=10):
-            if self.jumpCount >= -c:
-                i = 1
-                if self.jumpCount < 0:
-                    i = -1
-                    self.isFalling = True
-                self.yPos -= int(i * self.jumpCount ** 2  / 2)  # In the pygame the possition is a integer number // No pygame a posição deve ser dada em inteiros
-                self.jumpCount -= 1
-            else:
-                self.isFalling = False
-                self.inJump = False
-                self.jumpCount = c
-                self.stayCount = 2  # The sprite 2 is perfect for final of fall
+        if self.jumpCount >= -c:
+            i = 1
+            if self.jumpCount < 0:
+                i = -1
+                self.isFalling = True
+            self.yPos -= int(i * self.jumpCount ** 2 / 2)
+            # In the pygame the possition is a integer number // No pygame a posição deve ser dada em inteiros
+            self.jumpCount -= 1
+        else:
+            self.isFalling = False
+            self.inJump = False
+            self.jumpCount = c
+            self.stayCount = 2  # The sprite 2 is perfect for final of fall
 
 
 # Set characters
 
-cat = characther()
+cat = Character()
 
 
 # Game
 
 def debugging():
     stats = [
-        font.render("Jump: " + str(cat.inJump), True, (0, 255, 0)), 
-        font.render("Fall: " + str(cat.isFalling), True, (0, 255, 0)), 
-        font.render("Run: " + str(cat.isRunnig), True, (0, 255, 0)),
+        font.render("Jump: " + str(cat.inJump), True, (0, 255, 0)),
+        font.render("Fall: " + str(cat.isFalling), True, (0, 255, 0)),
+        font.render("Run: " + str(cat.isRunning), True, (0, 255, 0)),
         font.render("Dir: " + cat.faceDirection, True, (0, 255, 0)),
         font.render("Step Run: " + str(cat.stepCount), True, (0, 255, 0)),
         font.render("Step Jump: " + str(cat.jumpCount), True, (0, 255, 0)),
         font.render("Step Stay: " + str(cat.stayCount), True, (0, 255, 0)),
-        ]
+    ]
     for i, text in enumerate(stats):
-        screen.blit(text, ((win_size[0] - text.get_width()), i*(text.get_height() + 10)))
+        screen.blit(text, ((win_size[0] - text.get_width()), i * (text.get_height() + 10)))
 
 
-def redrawGameWindow():
-    
+def redraw_game_window():
     sprite_mode, sprite_act, sprite_index = 'simple', str(), int()
     if cat.isSuper:
         sprite_mode = 'super'
@@ -174,16 +178,17 @@ def redrawGameWindow():
         else:
             sprite_act = 'jump' + cat.faceDirection[0]
             sprite_index = int(cat.jumpCount % 4)
-    elif cat.isRunnig:
-            sprite_act = 'run' + cat.faceDirection[0]
-            sprite_index = cat.stepCount
+    elif cat.isRunning:
+        sprite_act = 'run' + cat.faceDirection[0]
+        sprite_index = cat.stepCount
     else:
         sprite_act = 'stay' + cat.faceDirection[0]
         sprite_index = cat.stayCount
-    
+
     screen.fill((255, 255, 255))
-    screen.blit(pygame.transform.scale(spritePack[sprite_mode][sprite_act][sprite_index], cat.size), (cat.xPos, cat.yPos - 4))  # "-4" por convenção do sprite
-    
+    screen.blit(pygame.transform.scale(spritePack[sprite_mode][sprite_act][sprite_index], cat.size),
+                (cat.xPos, cat.yPos - 4))  # "-4" por convenção do sprite
+
     # pygame.draw.rect(screen, cat.color, cat.get_all())
     debugging()
     pygame.display.update()
@@ -191,30 +196,31 @@ def redrawGameWindow():
 
 runGame = True
 while runGame:
-    
-    clock.tick(9) # FPS
-    
+
+    clock.tick(6)  # FPS
+
     # bye bye
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runGame = False
-    
+
     # movement
     keys = pygame.key.get_pressed()
-    
-    if (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]) and not (keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]):  # Xor Logic Gate: True if just one is True
+
+    if (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]) and not (
+            keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]):  # Xor Logic Gate: True if just one is True
         if keys[pygame.K_LEFT]:
             cat.step()
             if cat.xPos > 0:
                 cat.left()
-    
+
         elif keys[pygame.K_RIGHT]:
             cat.step()
             if cat.xPos + cat.w < win_size[0]:
-                cat.right()    
+                cat.right()
     elif not cat.inJump:
         cat.stay()
-    
+
     if not cat.inJump:
         ''' climbing
         if keys[pygame.K_UP] and cat.yPos > 0:
@@ -234,12 +240,12 @@ while runGame:
             cat.jumping()
     else:
         cat.jumping()
-    
+
     if keys[pygame.K_RSHIFT]:
         cat.super()
-    
+
     # Render
-    
-    redrawGameWindow()
+
+    redraw_game_window()
 
 pygame.quit()
